0eb8e84dea805cabe13367c35e2bd310bd643b36
villalbajoaquin <villalbajoaq@gmail.com>
2022-05-17 01:33:28 -0300

    feat: modify express app(app.js, views/[login, register, ...])

--- app.js
+++ app.js
@@ -13,6 +13,27 @@ app.listen(port, () => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
+    // index
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+    // product detail
+app.get('/product_detail', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetail.html'));
+});
+
+    // product cart
+app.get('/product_cart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productCart.html'));
+});
+
+    // register
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+    // login
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
 });
