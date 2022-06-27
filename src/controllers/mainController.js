7659093da172afaf92f9eae3eae4364ae01da596
villalbajoaquin <villalbajoaq@gmail.com>
2022-06-27 03:36:43 -0300

    feat: add sort-function for products @ productController.js & mainController.js

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -7,6 +7,11 @@ const productArray = require('../data/products.json');
 const mainController = {
     index: (req, res) => {
         let shows = productArray;
+        let showsPerDay = shows.sort((a, b) => {
+            let da = new Date(a.date);
+            let db = new Date(b.date);
+            return da - db;
+        });
         res.render('index', {shows: shows});
     }
 }
