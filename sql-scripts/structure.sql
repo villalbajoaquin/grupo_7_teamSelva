9afc14ecfeee1e0d2e4cf95d309c70b73488abcd
villalbajoaquin <villalbajoaq@gmail.com>
2022-08-02 18:55:43 -0300

    1st commit sequelize

---
+++ sql-scripts/structure.sql
@@ -0,0 +1,42 @@
+create database if not exists dbSelva;
+use dbSelva;
+drop table if exists products;
+create table products (
+	id int auto_increment,
+    name varchar(55) not null,
+    imgsrc varchar(55) not null,
+    date date not null,
+    time time not null,
+    tickets int not null,
+    price int not null,
+    primary key (id)
+);
+drop table if exists productsCart;
+create table productsCart (
+	id int auto_increment,
+    productId int not null,
+    quantity int not null,
+    price int not null,
+    cartId int not null,
+    primary key (id)
+);
+drop table if exists carts;
+create table carts (
+	id int auto_increment,
+    userId int not null,
+    productId int not null,
+    totalPrice int not null,
+    primary key (id)
+);
+drop table if exists users;
+create table users (
+	id int auto_increment,
+    firstName varchar(55) not null,
+    lastName varchar(55) not null,
+    email  varchar(55) not null,
+    password varchar(55) not null,
+    avatar varchar(55) not null,
+    categoryId int not null,
+    cud int null,
+    primary key (id)
+);
