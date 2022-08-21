0373b398b13cf1417c1b4f5fdfbdec5894db16b4
Urieluno <urielperrenfernandez@gmail.com>
2022-08-21 11:44:40 -0300

    add: models => users.js

---
+++ src/database/models/users.js
@@ -0,0 +1,55 @@
+module.exports = (sequelize, DataTypes)=>{
+
+    //Definicion del modelo usuario
+
+    const User = sequelize.define("Users", {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type:DataTypes.INTEGER
+        },
+        firstname:{
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        lastname:{
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        email:{
+            allowNull: false,
+            unique: true,
+            type: DataTypes.STRING
+        },
+        category:{
+            allowNull: false,
+            type: DataTypes.INTEGER
+        },
+        password:{
+            allowNull: false,
+            unique: true,
+            type: DataTypes.STRING
+        },
+        avatar:{
+            allowNull: false,
+            type: DataTypes.STRING
+        }
+     }, 
+        {
+        tableName: "users",
+        paranoid:true,
+        deletedAt: 'softDelete',
+        createdAt: 'createAt',
+        updatedAt: 'modifiedAt'
+    });
+
+    User.associate = (models) =>{
+
+        User.belongsTo(models.userCategory, 
+            {
+                as: "category",
+                foreignKey: "idUserCategory"
+            }
+        );
+        return User;
+}
