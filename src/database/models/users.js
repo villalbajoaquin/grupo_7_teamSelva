b3bad69429dc9b2c1dc4cb01bc14a49d7299ea70
vegaswalter <damianvegasrodriguez@gmail.com>
2022-09-04 15:37:55 -0300

    cartproduct fix

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -47,7 +47,7 @@ module.exports = (sequelize, DataTypes) => {
 
         User.belongsTo(models.userCategory,
             {
-                as: "category",
+                as: "1category",
                 foreignKey: "idUserCategory"
             }
         );
