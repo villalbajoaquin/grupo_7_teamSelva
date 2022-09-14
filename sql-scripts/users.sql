c7effb15a338465ffb9576b9064e48053a95d87e
villalbajoaquin <villalbajoaq@gmail.com>
2022-09-14 03:04:46 -0300

    fix: users.sql

--- sql-scripts/users.sql
+++ sql-scripts/users.sql
@@ -1,9 +1,10 @@
-use dbSelva
-insert into categories (firstName, lastName, email, password, avatar, categoryId)
+use dbSelva;
+insert into users (firstName, lastName, email, password, avatar, categoryId)
 values 
-	('Pedro',
-    'Gaston',
-    'Pedro@gmail.com',
-    '$2a$10$lh8vNlV5Em5tQVxTButtnuhj7UrE.g5N6v5C.lugxfaSCekW/rtfi',
-    'img/users/1663133810288_pdt_.jpg');
-select * from categories;
+	("Pedro",
+    "Gaston",
+    "Pedro@gmail.com",
+    "$2a$10$lh8vNlV5Em5tQVxTButtnuhj7UrE.g5N6v5C.lugxfaSCekW/rtfi",
+    "img/users/1663133810288_pdt_.jpg",
+    "2");
+select * from users;
