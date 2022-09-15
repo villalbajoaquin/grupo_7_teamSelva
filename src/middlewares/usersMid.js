e0f17b19eca9d37090fc121b72a5a9fa7b341663
Urieluno <urielperrenfernandez@gmail.com>
2022-09-15 12:38:48 -0300

    functional validations login / in process validation register: usersMid

--- src/middlewares/usersMid.js
+++ src/middlewares/usersMid.js
@@ -1,44 +1,78 @@
 const { body } = require('express-validator');
 const fs = require("fs");
 const path = require('path');
+const bcrypt = require('bcryptjs');
 const db = require("../database/models");
 
 const validationsRegister = [
-    body('firstname').notEmpty().withMessage('Por favor ingresa tu nombre'),
-    body('lastname').notEmpty().withMessage('Es necesario que completes tu apellido'),
-    body('email')
-        .notEmpty().withMessage('Por favor ingresa tu correo').bail()
-        .isEmail().withMessage('Por favor ingresa un correo válido'),
-    body('password').notEmpty().withMessage('No olvides tu contraseña')
-    .isLength({ min: 8 })
-    .withMessage("La contraseña debe de tener como mínimo 8 caracteres"),
-    body('passwordRe')
-        .notEmpty().withMessage('Por favor repite tu contraseña.').bail()
-        .isLength({ min: 8 })
-        .withMessage("La contraseña debe de tener como mínimo 8 caracteres")
-        .custom((value, { req }) => {
-            let passOriginal = req.body.password;
-            let nuevaPass = req.body.passwordRe;
-
-            if (passOriginal != nuevaPass) {
-                throw new Error('Las contraseñas no coinciden.');
-            }
-            return true;
-        }),
-    
-  body('avatar').custom((value, { req }) => {
-        let file = req.file;
-        let extensionesPermitidas = ['.png', '.jpg', '.jpeg', '.gif', '.svg', '.jfif'];
-        let fileExtension = path.extname(file.originalname);
-        if (!file) {
-            throw new Error('Seleccioná una imagen para tu producto');
-        } else {
-            if (!extensionesPermitidas.includes(fileExtension.toLowerCase())) {
-                throw new Error(`Las extensiones de archivo permitidas son ${extensionesPermitidas.join(", ")}`)
-            }
+     body("firstname")
+    .escape()
+    .trim()
+    .notEmpty()
+    .withMessage("El campo nombre no puede estar vacio")
+    .isLength({ min: 3 })
+    .withMessage("El nombre debe de tener al menos tres carácteres")
+    .isAlpha()
+    .withMessage("No estan permitidos los números o caracteres especiales")
+    ,
+    body("lastname")
+    .escape()
+    .trim()
+    .notEmpty()
+    .withMessage("El campo apellido no puede estar vacio")
+    .isLength({ min: 2 })
+    .withMessage("El apellido debe de tener al menos dos carácteres")
+    .isAlpha()
+    .withMessage("No estan permitidos los números o caracteres especiales")
+    ,
+    body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Este campo no puede estar vacio").bail()
+    .isEmail()
+    .withMessage("Debes ingresar un email válido")
+    .custom(async(value, {req})=>{
+      let match =
+        await db.Users.findOne({where:{email : req.body.email}})   
+      if(match){
+       throw new Error("Email actualmente en uso");
         }
         return true;
-    })
+      })
+       ,
+       body("password")
+       .trim()
+       .notEmpty()
+       .withMessage("El campo contraseña no debe estar vacio")
+       .isLength({ min: 8 })
+       .withMessage("La contraseña debe de tener como mínimo 8 caracteres"),
+       body("passwordRe")
+       .notEmpty()
+       .withMessage("El campo reingresar contraseña no debe estar vacio")
+       .trim()
+    
+       .custom(async (confirmPassword, {req}) => {
+         const password = req.body.password
+
+         if(password !== confirmPassword){
+           throw new Error('La contraseña no coincide con la ingresada')
+         }
+       }),
+    
+       body("avatar")
+       .custom((value, {req})=>{
+       let file = req.file;
+       let extensionesPermitidas = ['.png', '.jpg', '.jpeg', '.gif', '.svg', '.jfif'];
+       if(!file){
+         throw new Error('Debes de subir una imagen')
+       } else{
+         let fileExtension = path.extname(file.originalname);
+        if(!extensionesPermitidas.includes(fileExtension)){
+         throw new Error('Las extensiones permitidas son: ' + extensionesPermitidas.join(', '));
+       }
+       }
+       return true;
+     }),
 ];
 const validationsLogin = [
     body('email')
@@ -57,7 +91,20 @@ const validationsLogin = [
     body('password')
     .notEmpty().withMessage('No olvides tu contraseña')
     .isLength({ min: 8 })
-    .withMessage("La contraseña debe de tener como mínimo 8 caracteres"),
+    .withMessage("La contraseña debe de tener como mínimo 8 caracteres")
+    .custom( async (value, {req})=>{
+        let users = await db.Users.findOne({
+            where: {email: req.body.email}
+        })
+        if(users == undefined){
+            throw new Error("Primero debes ingresar un email válido");
+        }
+        let secure = await bcrypt.compare(req.body.password, users.password) 
+        if(!secure){
+            throw new Error("La contraseña es incorrecta");
+                   }
+                return true;
+        })
 ];
 
 
