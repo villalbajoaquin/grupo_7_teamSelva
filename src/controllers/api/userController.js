60656e9ad3d4fa102b211b54535549ffa6ee938e
vegaswalter <damianvegasrodriguez@gmail.com>
2022-09-15 23:43:18 -0300

    API arrangement

--- src/controllers/api/userController.js
+++ src/controllers/api/userController.js
@@ -1,4 +1,4 @@
-const db = require("../../database/models");
+/*const db = require("../../database/models");
 
 const  userController = {
 
--- src/routes/api/user.routes.js
+++ src/routes/api/user.routes.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const userController = require("../../controllers/api/userController");
 const userRoutes = express.Router();
 
