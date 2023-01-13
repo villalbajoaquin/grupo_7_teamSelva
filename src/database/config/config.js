fb060904cf81e70b6efafeb46c6bea60eae9b422
villalbajoaquin <villalbajoaq@gmail.com>
2023-01-13 03:02:24 -0300

    chore: config fix

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -20,10 +20,14 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "port": process.env.DB_PORT,
+    "define": {
+      "timestamps": false
+    },
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
